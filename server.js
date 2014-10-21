1104214566ca160bd7eb94c67cc9d26d87b5cb96
Maru <maruthi.turaga@gmail.com>
2014-10-21 21:00:28 +0000

    update the jade html

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require("express");
 var app = express();
 
 app.set("views", __dirname)
-app.set("view engine", "jade");
+//app.set("view engine", "html");
 
 app.use(express.static(__dirname + "/public"))
 
 app.get("*", function(req, res){
-    res.render("index");
+    res.render("index.html");
 });
 
 app.listen(process.env.PORT, process.env.IP);
