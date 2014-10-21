f11c21bfbbd8307edd051fc6b5e855aa22299d9d
Maru <maruthi.turaga@gmail.com>
2014-10-21 19:45:02 +0000

    root

---
+++ public/app/app.js
@@ -0,0 +1,11 @@
+angular.module("app", []);
+
+angular.module("app").controller("testCtrl", function($scope) {
+    $scope.jobs = [{
+        title: "Sales Manager",
+        description: "lorem ipsum"
+    }, {
+        title: "CEO",
+        description: "The Boss"
+    }];
+});
