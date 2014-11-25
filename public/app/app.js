7c9a309da225668639e49e1e8b18b5a4da7f0bdb
Maru <maruthi.turaga@gmail.com>
2014-11-25 16:24:11 +0000

    update

--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,32 @@
-angular.module("app", []);
+(function() {
+    angular.module("app", ['app-directives', 'mdb-directives']);
+    
 
-angular.module("app").controller("testCtrl", function($scope) {
-    $scope.jobs = [{
-        title: "Sales Manager",
-        description: "lorem ipsum",
-        tags: ["Sales","Talking Bs"]
-    }, {
-        title: "CEO",
-        description: "The Boss",
-        tags: ["Boss"]
-    },{
-        title: "CFO",
-        description: "The Bean Counter",
-        tags: ["count it","Hell"]
-    }];
-});
+    angular.module("app").controller("testCtrl", function($scope) {
+        $scope.jobs = [{
+            title: "Sales Manager",
+            description: "lorem ipsum",
+            tags: ["Sales", "Talking Bs"]
+        }, {
+            title: "CEO",
+            description: "The Boss",
+            tags: ["Boss"]
+        }, {
+            title: "CFO",
+            description: "The Bean Counter",
+            tags: ["count it", "Hell"]
+        }];
+
+
+        $scope.categories = [{
+            ID: 1,
+            Name: "Cat1"
+        }, {
+            ID: 2,
+            Name: "Cat2"
+        }, {
+            ID: 3,
+            Name: "Cat3"
+        }];
+    });
+})();
